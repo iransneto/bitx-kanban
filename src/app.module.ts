7a6d11bec9d4a9f0a46c408ac3fc827d9b255b61
Iran S. Neto <iransneto2@gmail.com>
2023-09-30 13:00:03 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { StatusModule } from './status/status.module';
+import { CategoriesModule } from './categories/categories.module';
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
+import { RolesModule } from './roles/roles.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    DatabaseModule,
+    StatusModule,
+    CategoriesModule,
+    TasksModule,
+    UsersModule,
+    CommentsModule,
+    RolesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
